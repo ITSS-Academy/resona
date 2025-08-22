aa002e1a20081e8b3e0d584ff18e9d639613a856
Hà Hiệp Thanh <hahiepthanhhhtt@gmail.com>
2025-08-22 17:19:59 +0700

    ADDED: Full album, song queue

--- src/app/components/queue/queue.component.ts
+++ src/app/components/queue/queue.component.ts
@@ -1,8 +1,11 @@
 import { Component } from '@angular/core';
+import {PlaybarSongDetailComponent} from '../queue-song-detail/playbar-song-detail.component';
 
 @Component({
   selector: 'app-queue',
-  imports: [],
+  imports: [
+    PlaybarSongDetailComponent
+  ],
   templateUrl: './queue.component.html',
   styleUrl: './queue.component.scss'
 })
--- src/app/pages/song-detail/song-detail.component.ts
+++ src/app/pages/song-detail/song-detail.component.ts
@@ -1,12 +1,22 @@
 import { Component } from '@angular/core';
 import {PlayerBarComponent} from '../../components/player-bar/player-bar.component';
 import {AlbumCardComponent} from '../../components/album-card/album-card.component';
+import {MatDivider, MatList, MatListItem} from '@angular/material/list';
+import {FullAlbumDetailComponent} from '../../components/full-album-detail/full-album-detail.component';
+import {QueueComponent} from '../../components/queue/queue.component';
+import {PlaybarSongDetailComponent} from '../../components/queue-song-detail/playbar-song-detail.component';
 
 @Component({
   selector: 'app-song-detail',
   imports: [
     PlayerBarComponent,
-    AlbumCardComponent
+    AlbumCardComponent,
+    MatList,
+    MatListItem,
+    MatDivider,
+    FullAlbumDetailComponent,
+    QueueComponent,
+    PlaybarSongDetailComponent
   ],
   templateUrl: './song-detail.component.html',
   styleUrl: './song-detail.component.scss'
