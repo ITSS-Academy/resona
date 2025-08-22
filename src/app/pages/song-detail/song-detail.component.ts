df1395a8d595d4e764239ad039c620725e82b0fa
Hà Hiệp Thanh <hahiepthanhhhtt@gmail.com>
2025-08-22 13:11:57 +0700

    ADDED: album card component

--- src/app/pages/song-detail/song-detail.component.ts
+++ src/app/pages/song-detail/song-detail.component.ts
@@ -1,8 +1,13 @@
 import { Component } from '@angular/core';
+import {PlayerBarComponent} from '../../components/player-bar/player-bar.component';
+import {AlbumCardComponent} from '../../components/album-card/album-card.component';
 
 @Component({
   selector: 'app-song-detail',
-  imports: [],
+  imports: [
+    PlayerBarComponent,
+    AlbumCardComponent
+  ],
   templateUrl: './song-detail.component.html',
   styleUrl: './song-detail.component.scss'
 })
