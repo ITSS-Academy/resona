27c806af2026c156a5806cc7bfc659ad7b45d5f2
Thành Thu <thuvoong@gmail.com>
2025-09-10 16:01:48 +0700

    feat: redesign category detail component with new layout and improved track display

--- src/app/pages/category-detail/category-detail.component.ts
+++ src/app/pages/category-detail/category-detail.component.ts
@@ -1,67 +1,61 @@
-import { Component, Input, OnDestroy, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { Store } from '@ngrx/store';
-import { Observable, Subscription } from 'rxjs';
-import { TrackModel } from '../../models/track.model';
-import {AsyncPipe, DatePipe, DecimalPipe} from '@angular/common';
-import { TrackState } from '../../ngrx/track/track.state';
-import * as TrackActions from '../../ngrx/track/track.action';
-import { ImgConverterPipe } from '../../shared/pipes/img-converter.pipe';
+import { map, Observable, Subscription } from 'rxjs';
 import { CategoryModel } from '../../models/category.model';
+import { TrackModel } from '../../models/track.model';
 import { CategoryState } from '../../ngrx/category/category.state';
 import * as CategoryActions from '../../ngrx/category/category.action';
-import { MusicTabComponent } from '../../components/music-tab/music-tab.component';
-import {DurationPipe} from '../../shared/pipes/duration.pipe';
-import {MatIconModule} from '@angular/material/icon';
+import { AsyncPipe } from '@angular/common';
+import { MaterialModule } from '../../shared/modules/material.module';
+import { PlaylistMusicTabComponent } from '../../components/playlist-music-tab/playlist-music-tab.component';
+import { PlaylistDetailButtonComponent } from '../../components/playlist-detail-button/playlist-detail-button.component';
 
 @Component({
   selector: 'app-category-detail',
-  standalone: true,
   templateUrl: './category-detail.component.html',
-  imports: [ MusicTabComponent, MatIconModule],
   styleUrls: ['./category-detail.component.scss'],
+  standalone: true,
+  imports: [
+    AsyncPipe,
+    MaterialModule,
+    PlaylistMusicTabComponent,
+    PlaylistDetailButtonComponent,
+  ],
 })
 export class CategoryDetailComponent implements OnInit, OnDestroy {
-  tracks$!: Observable<TrackModel[]>;
-  tracks: TrackModel[] = [];
-  subscriptions: Subscription[] = [];
-  category$!: Observable<CategoryModel>;
-  category!: CategoryModel;
+  categoryId!: string;
+  categoryDetail$!: Observable<CategoryModel | null>;
+  tracks$!: Observable<TrackModel[] | undefined>;
+  categoryName: string = '';
+
+  private subscriptions = new Subscription();
 
   constructor(
-    private activatedRoute: ActivatedRoute,
-    private store: Store<{
-      category: CategoryState;
-      track: TrackState;
-    }>
-  ) {
-    const { id } = this.activatedRoute.snapshot.params;
-
-    this.store.dispatch(TrackActions.getTrackByCategoryId({ categoryId: id }));
-    this.store.dispatch(CategoryActions.getCategoryDetails({ categoryId: id }));
-
-    this.tracks$ = this.store.select('track', 'tracks');
-    this.category$ = this.store.select('category', 'category');
-  }
+    private route: ActivatedRoute,
+    private store: Store<{ category: CategoryState }>
+  ) {}
 
   ngOnInit() {
-    this.subscriptions.push(
-      this.tracks$.subscribe((tracks) => {
-        this.tracks = tracks;
-      }),
-      this.category$.subscribe((category) => {
-        this.category = category;
-      })
-    );
+    const routeSub = this.route.params.subscribe(params => {
+      this.categoryId = params['id'];
+      if (this.categoryId) {
+        this.store.dispatch(CategoryActions.getCategoryDetails({ categoryId: this.categoryId }));
+
+        this.categoryDetail$ = this.store.select(state => state.category.category);
+
+        this.tracks$ = this.categoryDetail$.pipe(map(c => c?.tracks));
+
+        const categoryNameSub = this.categoryDetail$.subscribe(c => {
+          this.categoryName = c?.name || '';
+        });
+        this.subscriptions.add(categoryNameSub);
+      }
+    });
+    this.subscriptions.add(routeSub);
   }
 
   ngOnDestroy() {
-    this.subscriptions.forEach((sub) => sub.unsubscribe());
+    this.subscriptions.unsubscribe();
   }
-
-  onPlayTrack(track: TrackModel) {
-    console.log('Play track:', track);
-    // this.store.dispatch(TrackActions.playTrack({ track }));
-  }
-
 }
